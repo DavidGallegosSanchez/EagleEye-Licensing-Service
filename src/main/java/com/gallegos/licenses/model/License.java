f96e61cd03109c11f9f93f7934fcf17324f93e2b
David Gallegos <dgallegos1985@gmail.com>
2025-01-08 16:30:17 +0100

    feat: adding business implementation for Licenses

--- src/main/java/com/gallegos/licenses/model/License.java
+++ src/main/java/com/gallegos/licenses/model/License.java
@@ -1,23 +1,46 @@
 package com.gallegos.licenses.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "licenses")
 public class License {
 
-    private String id;
+    @Id
+    @Column(name = "license_id", nullable = false)
+    private String licenseId;
+
+    @Column(name = "organization_id", nullable = false)
     private String organizationId;
+
+    @Column(name = "product_name", nullable = false)
     private String productName;
+
+    @Column(name = "license_type", nullable = false)
     private String licenseType;
 
+    @Column(name = "license_max", nullable = false)
+    private Integer licenseMax;
+
+    @Column(name = "license_allocated", nullable = false)
+    private Integer licenseAllocated;
+
+    @Column(name="comment")
+    private String comment;
+
     public License withId(String id){
-        this.setId( id );
+        this.setLicenseId(id);
         return this;
     }
 
@@ -36,4 +59,18 @@ public class License {
         return this;
     }
 
+    public License withLicenseMax(Integer licenseMax){
+        this.setLicenseMax(licenseMax);
+        return this;
+    }
+
+    public License withLicenseAllocated(Integer licenseAllocated){
+        this.setLicenseAllocated(licenseAllocated);
+        return this;
+    }
+
+    public License withComment(String comment){
+        this.setComment(comment);
+        return this;
+    }
 }
